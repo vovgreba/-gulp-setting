3ef9e730ec55be7ee3996d00930cb23c578c409c
vovgreba <49303544+vovgreba@users.noreply.github.com>
2020-07-09 14:36:43 +0300

    add section controll and 404 error fonts

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+// --------- activate WOW --------------
+new WOW().init();
+
+
+
 $(document).ready(function() {
     $('#modal').click(function() {
         $('.overlay').fadeIn(500);
@@ -5,7 +10,25 @@ $(document).ready(function() {
     $('.modal__close').click(function() {
         $('.overlay').fadeOut(500);
     });
+    var mySwiper = new Swiper ('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        loop: true
+        
+      })
+    var prev = $('.swiper-button-prev'),
+        next = $('.swiper-button-next'),
+        bullets = $('.swiper-pagination');
     
+    bullets.css('left', prev.width() + 20)
+    next.css('left', prev.width() + 20 + bullets.width() + 20)
+
 });
 
 $(document).mouseup(function(e) {
@@ -26,6 +49,18 @@ $(document).ready(function() {
             $('#jsScrollTop').addClass('is-show');
         } else {
             $('#jsScrollTop').removeClass('is-show');
+        }
+    });
+
+    // Валидация формы
+
+    $('.modal__form').validate({
+        rules: {
+            userName: "required",
+            userEmail: {
+              required: true,
+              email: true
+            }
         }
     });
 });
